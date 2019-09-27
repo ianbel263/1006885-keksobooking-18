73831afd7929d357d7d9a218c2132d2cde91ff43
Andrey Belyaev <ianbel263@gmail.com>
2019-09-27 12:36:06 +0300

    Удаляет выводы в консоль

--- js/main.js
+++ js/main.js
@@ -215,14 +215,11 @@ var checkCapacityValidity = function () {
   if (parseInt(selectRoomNumber.value, 10) < parseInt(selectCapacity.value, 10)) {
     if (parseInt(selectRoomNumber.value, 10) !== 100 || parseInt(selectCapacity.value, 10) !== 0) {
       selectCapacity.setCustomValidity('Количество гостей не должно быть больше количества комнат');
-      console.log("parseInt(selectRoomNumber.value, 10)", parseInt(selectRoomNumber.value, 10));
     }
   } else if (parseInt(selectRoomNumber.value, 10) === 100 && parseInt(selectCapacity.value, 10) !== 0) {
     selectCapacity.setCustomValidity('Для 100 комнат выберите значение "не для гостей"');
-    console.log("parseInt(selectRoomNumber.value, 10)", parseInt(selectRoomNumber.value, 10));
   } else if (parseInt(selectRoomNumber.value, 10) !== 100 && parseInt(selectCapacity.value, 10) === 0) {
     selectCapacity.setCustomValidity('Если выбираете "не для гостей", укажите количство комнат, равное 100');
-    console.log("parseInt(selectRoomNumber.value, 10)", parseInt(selectRoomNumber.value, 10));
   } else if (parseInt(selectRoomNumber.value, 10) === 100 && parseInt(selectCapacity.value, 10) === 0) {
     selectCapacity.setCustomValidity('');
   } else {
