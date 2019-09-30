0b648a6367e29c3987a800d4e802d0b96ec2a717
Andrey Belyaev <ianbel263@gmail.com>
2019-09-30 15:09:28 +0300

    Разбивает код на модули

---
+++ js/pin.js
@@ -0,0 +1,23 @@
+'use strict';
+
+(function () {
+  var mapPin = window.data.map.querySelector('.map__pins');
+  var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+
+  var renderPin = function (pin) {
+    var pinElement = pinTemplate.cloneNode(true);
+    pinElement.style = 'left: ' + pin.location.x + 'px; top: ' + pin.location.y + 'px;';
+    pinElement.querySelector('img').src = pin.author.avatar;
+    pinElement.querySelector('img').alt = pin.offer.title;
+
+    return pinElement;
+  };
+
+  window.renderPins = function (arr) {
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < arr.length; i++) {
+      fragment.appendChild(renderPin(arr[i]));
+    }
+    mapPin.appendChild(fragment);
+  };
+})();
