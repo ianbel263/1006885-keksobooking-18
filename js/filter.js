6d5de7f615d954dc5612315e4d4eb6a6b30984bb
Andrey Belyaev <ianbel263@gmail.com>
2019-10-10 15:40:42 +0300

    Добавляет фильтры по цене, количеству комнат и гостей

--- js/filter.js
+++ js/filter.js
@@ -3,17 +3,62 @@
 (function () {
   var ads = [];
 
+  var Price = {
+    MIN: 10000,
+    MAX: 50000,
+    LOW: 'low',
+    MIDDLE: 'middle',
+    HIGH: 'high'
+  };
+
+  var roomsGuestsToValue = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '0': 0
+  };
+
   var filterForm = document.querySelector('.map__filters');
   var selectType = filterForm.querySelector('#housing-type');
+  var selectPrice = filterForm.querySelector('#housing-price');
+  var selectRooms = filterForm.querySelector('#housing-rooms');
+  var selectGuests = filterForm.querySelector('#housing-guests');
 
   var getType = function (element) {
     return selectType.value === 'any' ? true : element.offer.type === selectType.value;
   };
 
+  var getPrice = function (element) {
+    switch (selectPrice.value) {
+      case Price.LOW:
+        return element.offer.price < Price.MIN;
+      case Price.MIDDLE:
+        return element.offer.price >= Price.MIN && element.offer.price <= Price.MAX;
+      case Price.HIGH:
+        return element.offer.price > Price.MAX;
+      default:
+        return true;
+    }
+  };
+
+  var getRooms = function (element) {
+    return selectRooms.value === 'any' ? true : element.offer.rooms === roomsGuestsToValue[selectRooms.value];
+  };
+
+  var getGuests = function (element) {
+    return selectGuests.value === 'any' ? true : element.offer.guests === roomsGuestsToValue[selectGuests.value];
+  };
+
+  // var getFeatures = function (element) {
+  //   // body...
+  // };
+
   var filterData = function (data) {
-    console.log("data", data);
     return data.filter(function (el) {
-      return  getType(el);
+      return  getType(el) &&
+              getPrice(el) &&
+              getRooms(el) &&
+              getGuests(el);
     }).slice(0, 5);
   };
 
